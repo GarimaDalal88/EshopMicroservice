dcea4b7caf2fbab7c85ff9b5f87036a628f2eff7
GarimaDalal88 <151963720+GarimaDalal88@users.noreply.github.com>
2025-04-18 23:46:42 +0530

    Adding the remaining files for Catalog API and Building Block class library

--- src/Services/Catalog/CatalogAPI/ProductsFeatures/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/ProductsFeatures/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,37 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+using System.Windows.Input;
 
 namespace CatalogAPI.ProductsFeatures.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //Business logic to create a product
-            throw new NotImplementedException();
+            //create Product entity from command Project
+
+            //save to database
+
+            //return createProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //TODO
+            //save to database
+            //return result
+
+            return new CreateProductResult(Guid.NewGuid());
+
         }
     }
 }
