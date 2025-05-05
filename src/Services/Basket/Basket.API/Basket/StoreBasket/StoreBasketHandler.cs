6e622a0c0ee6511d6d473038fb8c44860b02ad06
GarimaDalal88 <151963720+GarimaDalal88@users.noreply.github.com>
2025-05-05 23:21:28 +0530

    Latest changes to GIt

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+    public record StoreBasketResult(bool IsSuccess);
+
+    public class StoreBasketCommandValidator: AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    
+
+    public class StoreBasketHandler
+    {
+    }
+}
