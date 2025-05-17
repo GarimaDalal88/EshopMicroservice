214e38aa1d7ae6848e69d9a529c3385669f2c42a
GarimaDalal88 <151963720+GarimaDalal88@users.noreply.github.com>
2025-05-17 22:58:43 +0530

    Ordering micrsoervice started

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!; 
+    }
+}
