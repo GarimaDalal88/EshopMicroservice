a41c793adfe037371fc7ea25f0bffb6641f79034
GarimaDalal88 <151963720+GarimaDalal88@users.noreply.github.com>
2025-05-20 23:03:57 +0530

    Ordering.Infrastructure API completed

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomers/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomers
+{
+    public record GetOrdersByCustomerQuery(Guid CustomerId)
+        :IQuery<GetOrdersByCustomerResult>;
+
+    public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
+}
