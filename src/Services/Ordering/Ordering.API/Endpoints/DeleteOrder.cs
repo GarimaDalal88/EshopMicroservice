3cbae82444691062e6f71c7ef0ef1f28285c11c8
GarimaDalal88 <151963720+GarimaDalal88@users.noreply.github.com>
2025-05-21 20:22:06 +0530

    Orders.API Endpoints Completed

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,33 @@
+﻿
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints
+{
+    //Accept the order ID as a parameter
+    //Construct a DeleteOrderCommand
+    //Send the command using MediatR
+    //Returns a sucess or not found response
+
+    //public record DeleteOrderRequest(Guid Id);
+    public record DeleteOrderResponse(bool IsSucess);
+    public class DeleteOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteOrderCommand(Id));
+
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("Delete Order")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order");
+        }
+    }
+}
