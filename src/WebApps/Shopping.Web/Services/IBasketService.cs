21a6f957f78f1c57c214b31b2cfbe1a4360aed92
GarimaDalal88 <151963720+GarimaDalal88@users.noreply.github.com>
2025-05-24 02:24:57 +0530

    Web client completed - unit testing for entire application pending

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,40 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface IBasketService
+    {
+        [Get("/basket-service/basket/{userName}")]
+        Task<GetBasketResponse> GetBasket(string userName);
+
+        [Post("/basket-service/basket")]
+        Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+        [Delete("/basket-service/basket/{userName}")]
+        Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+        [Post("/basket-service/basket/checkout")]
+        Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+        public async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            //get Basket if not exist create
+            var userName = "swn";
+            ShoppingCartModel basket;
+
+            try
+            {
+                var getBasketResponse = await GetBasket(userName);
+                basket = getBasketResponse.Cart;
+            }
+            catch (ApiException apiexception) when (apiexception.StatusCode == System.Net.HttpStatusCode.NotFound) 
+            {
+                basket = new ShoppingCartModel
+                {
+                    UserName = userName,
+                    Items = []
+                };
+            }
+
+            return basket;
+        }
+    }
+}
